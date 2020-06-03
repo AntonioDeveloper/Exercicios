2324e54ebbb81b63796c0b27d187978463d682c2
AntonioDeveloper <37427385+AntonioDeveloper@users.noreply.github.com>
2020-06-02 21:24:06 -0300

    Add files via upload

--- Desafios-JS/maisMenos.js
+++ Desafios-JS/maisMenos.js
@@ -2,21 +2,26 @@ let pos = 0;
 let neg = 0;
 let zeros = 0;
 let valores = [];
+let res = [];
+let porctNeg, porctZeros, porctPos = 0;
 
 function maisMenos(valores) {
-  for (var i = 0; i < valores; i++) {
+  for (var i = 0; i < valores.length; i++) {
     if (valores[i] < 0) {
       neg = neg + 1;
-    };
-    if (valores[i] == 0) {
+    }
+    else if (valores[i] == 0) {
       zeros = zeros + 1
     } else {
       pos = pos + 1;
     }
-  }
-  console.log(neg);
-  console.log(zeros);
-  console.log(pos);
+  };
+  porctNeg = (neg * 1) / valores.length;
+  porctZeros = (zeros * 1) / valores.length;
+  porctPos = (pos * 1) / valores.length;
+  res.push(porctPos, porctZeros, porctNeg);
+
+  return res;
 };
 
-maisMenos([1, 2, -4, 0, 0, -8, -13, 14]);
+console.log(maisMenos([1, 2, 0, -1]));
