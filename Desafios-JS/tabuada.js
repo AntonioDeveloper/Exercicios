86c47bd16f86c7a2836a87ff252df87212a95c50
AntonioDeveloper <37427385+AntonioDeveloper@users.noreply.github.com>
2020-10-07 18:04:14 -0300

    Add files via upload

---
+++ Desafios-JS/tabuada.js
@@ -0,0 +1,11 @@
+let res = null;
+let number = null;
+
+for(let j = 3; j < 7; j++){
+for(let i = 0; i < 11; i++){
+number = j;
+res = number * i;
+document.write( res + "<br>");
+};
+document.write( "<br>");
+}
